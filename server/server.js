9f5438851953e69ecc3f3f3cbd7abcb8e8ff1878
Michael Hamm <mikejameshamm@gmail.com>
2019-02-23 10:22:55 -0800

    Enable gzip compression

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var compression = require('compression');
 var app = express();
 var path = require('path');
 var port = 3000;
 
 app.set('port', port);
-
+app.use(compression());
 app.use(express.static(path.join(__dirname, '../client')));
 
 app.listen(app.get('port'));
